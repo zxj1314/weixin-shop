12702aad643f64c750ceabcf649fbc7ffe44c56e
zxj <961310225@qq.com>
2018-03-21 10:59:59 +0800

    Initial commit

---
+++ src/main/java/com/cbd/weixin/mapper/ClientMapper.java
@@ -0,0 +1,22 @@
+package com.cbd.weixin.mapper;
+
+
+import com.cbd.weixin.domain.Client;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+/**
+ * Created by zlb on 2017.09.05.
+ */
+public interface ClientMapper {
+	void insert(Client client);
+
+	void delete(Long id);
+
+	void update(Client client);
+
+	//查询取消关注的客户的id
+	Long getClientId(String openID);
+
+	List<Client> listAll();
+}
